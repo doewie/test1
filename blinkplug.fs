5507c62cb3cadc308a9b346b2799fd0d11a7df4f
doewie <douwe@laranja.nl>
2016-04-23 08:57:44 +0100

    initial template

---
+++ blinkplug.fs
@@ -0,0 +1,34 @@
+\ development code
+\ based on the 'ztw' project of JeeLabs
+\ The STM32F103 is preloaded with the h/l files of this project
+
+\ test-app for the JeeLabs BlinkPlug.
+\ the BlinkPlug is connected as follows:
+\ green LED to port
+\ red LED to port
+
+\ also re: 
+\	http://jeelabs.net/projects/hardware/wiki/Blink_Plug
+\	
+
+\ remove ram-compiled stuff and reset Forth system
+reset
+
+\ include application specific source files
+include version.fs
+
+: setup ( -- )
+	." Started. Press key to stop..."
+;
+
+: mainloop ( -- ) 			\ endless loop until key pressed
+	begin 
+		key? 
+	until
+;
+
+: main ( -- )
+	ver           					\ show version
+	setup
+	mainloop						\ start main loop
+;
---
+++ version.fs
@@ -0,0 +1,11 @@
+\ define version string
+\ TODO: let git change the content of this file.
+
+: ver. ( -- )  \ output version info
+	CR CR
+	." **********************" CR
+	." * name   : BlinkPlug *" CR
+	." * version: 0.1       *" CR
+	." **********************" CR
+	CR
+;
