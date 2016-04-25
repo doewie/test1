2827146dbb74ee25c74b18eda508b1d36a2f35fe
doewie <douwe@laranja.nl>
2016-04-25 09:12:47 +0100

    rename to NVIC.x registernames and rem old stuff for reference

--- interrupt.fs
+++ interrupt.fs
@@ -12,38 +12,50 @@
 
 \ ----------------------------
 \  Interrupt controller tools
-\ ----------------------------
+\ ---------------------------
+\ -
+\ Nested Vector Interrupt Controller (NVIC)
 
-$E000E100 constant en0 ( Interrupt Set Enable  0-31  )
-$E000E104 constant en1 ( Interrupt Set Enable 32-63  )
-$E000E108 constant en2 ( Interrupt Set Enable 64-95  )
-$E000E10C constant en3 ( Interrupt Set Enable 96-127 )
+$E000E100 constant NVIC				\ base
 
-$E000E180 constant dis0 ( Interrupt Clear Enable  0-31  )
-$E000E184 constant dis1 ( Interrupt Clear Enable 32-63  )
-$E000E188 constant dis2 ( Interrupt Clear Enable 64-95  )
-$E000E18C constant dis3 ( Interrupt Clear Enable 96-127 )
+	NVIC $00 + constant NVIC.ISER0	\ Interrupt Set Enable ( # 00..31 )
+	NVIC $04 + constant NVIC.ISER1	\ Interrupt Set Enable ( # 32..63 )
+
+	NVIC $80 + constant NVIC.ICER0	\ Interrupt Clear Enable ( # 00..31 )
+	NVIC $80 + constant NVIC.ICER1	\ Interrupt Clear Enable ( # 32..63 )
+	
+
+\ $E000E100 constant en0 ( Interrupt Set Enable  0-31  )
+\ $E000E104 constant en1 ( Interrupt Set Enable 32-63  )
+\ $E000E108 constant en2 ( Interrupt Set Enable 64-95  )
+\ $E000E10C constant en3 ( Interrupt Set Enable 96-127 )
+
+\ $E000E180 constant dis0 ( Interrupt Clear Enable  0-31  )
+\ $E000E184 constant dis1 ( Interrupt Clear Enable 32-63  )
+\ $E000E188 constant dis2 ( Interrupt Clear Enable 64-95  )
+\ $E000E18C constant dis3 ( Interrupt Clear Enable 96-127 )
 
 
-: nvic-enable ( irq# -- )
-  16 - \ Cortex Core Vectors
-  dup 32 u< if      1 swap lshift en0 ! exit then
-  dup 64 u< if 32 - 1 swap lshift en1 ! exit then
-  dup 96 u< if 64 - 1 swap lshift en2 ! exit then
-               96 - 1 swap lshift en3 !
-;
+\ : nvic-enable ( irq# -- )
+\   16 - \ Cortex Core Vectors
+\   dup 32 u< if      1 swap lshift NVIC.ISER0 ! exit then
+\   dup 64 u< if 32 - 1 swap lshift en1 ! exit then
+\   dup 96 u< if 64 - 1 swap lshift en2 ! exit then
+\                96 - 1 swap lshift en3 !
+\ ;
 
-: nvic-disable ( irq# -- )
-  16 - \ Cortex Core Vectors
-  dup 32 u< if      1 swap lshift dis0 ! exit then
-  dup 64 u< if 32 - 1 swap lshift dis1 ! exit then
-  dup 96 u< if 64 - 1 swap lshift dis2 ! exit then
-               96 - 1 swap lshift dis3 !
-;
+\ : nvic-disable ( irq# -- )
+\   16 - \ Cortex Core Vectors
+\   dup 32 u< if      1 swap lshift dis0 ! exit then
+\   dup 64 u< if 32 - 1 swap lshift dis1 ! exit then
+\   dup 96 u< if 64 - 1 swap lshift dis2 ! exit then
+\                96 - 1 swap lshift dis3 !
+\ ;
  
-: nvic-priority ( priority irq# -- )
-  $E000E400 + c!
-;
+\ : nvic-priority ( priority irq# -- )
+\   $E000E400 + c!
+\ ;
+
 
 : HANDLE.INT	\ handling of the interrupt on EXTI1
 	." x" CR	\ display x on every interrupt
@@ -74,6 +86,6 @@ $E000E18C constant dis3 ( Interrupt Clear Enable 96-127 )
 	\ enable interrupt in the mask register
 	4 bit EXTI.IMR bis!			\ set bit 4
 
-	\ 7 nvic-enable				\ enable interrupt
-	10 bit en0 bis!              \ enable bit 10 in ISER
+	\ 10 nvic-enable			\ enable interrupt TO BE TESTED !!!!!!!
+	10 bit NVIC.ISER0 bis! 		\ enable bit 10 in ISER
 ;
