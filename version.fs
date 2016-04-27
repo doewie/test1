5867570bd49ea4565f012f676484715a9d14ffab
doewie <douwe@laranja.nl>
2016-04-27 09:38:54 +0100

    include DEBOUNCE.DELAY below version info

--- version.fs
+++ version.fs
@@ -2,10 +2,13 @@
 \ TODO: let git change the content of this file.
 
 : VER. ( -- )  \ output version info
-	CR CR
-	." **********************" CR
-	." * name   : BlinkPlug *" CR
-	." * version: 0.3       *" CR
-	." **********************" CR
+	CR
+	CR ." name   : BlinkPlug"
+	CR ." version: 0.3"
+	CR
+	CR	." debounce delay: "
+		DEBOUNCE.DELAY @ .
+		."  ms"
+	CR
 	CR
 ;
