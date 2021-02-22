48ddcaa29d8e4146f3ca4b4a111354fc8fb01b61
Douwe Brouwer <douwe@laranja.nl>
2021-02-22 10:10:30 +0100

    Update version.fs

--- version.fs
+++ version.fs
@@ -1,14 +1,1 @@
-\ define version string
-\ TODO: let git change the content of this file.
-
-: VER. ( -- )  \ output version info
-	CR
-	CR ." name   : BlinkPlug"
-	CR ." version: 0.3"
-	CR
-	CR	." debounce delay: "
-		DEBOUNCE.DELAY @ .
-		."  ms"
-	CR
-	CR
-;
+: HASH $23857fe ;
